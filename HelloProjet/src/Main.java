df076ece24a7d93f45fa872201f44c347bb71d94
cuijian <2697378583@qq.com>
2019-12-18 14:47:02 +0800

    第二次提交

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -1,10 +1,7 @@
 import org.junit.Test;
 
 public class Main {
-//dfsdfsdfsd
-//这是主干代码
-	//dffsdhf
-	//这是分支
+
     @Test
     public void testHelloWrold() {
         System.out.println("HelloWorld!");
